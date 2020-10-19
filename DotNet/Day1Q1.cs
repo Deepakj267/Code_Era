cd52c0a36bc7d01a8bcb3483be994684c9b2662d
Deepakj267 <63543321+Deepakj267@users.noreply.github.com>
2020-10-19 17:35:58 +0530

    Add files via upload

---
+++ DotNet/Day1Q1.cs
@@ -0,0 +1,18 @@
+//Q1. Declare 2 variable of a type short and store sum of it in a variable and print it.
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            short x = 10;
+            short y = 20;
+            int z=x +y;
+
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }  
+}
