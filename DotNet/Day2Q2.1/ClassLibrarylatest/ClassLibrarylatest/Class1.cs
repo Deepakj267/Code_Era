2e41264ab223ca0631df10ee6fdd56a0fa9ee7aa
Deepak Ashokrao Jagtap <63543321+Deepakj267@users.noreply.github.com>
2020-10-21 18:22:35 +0530

    Add files via upload

---
+++ DotNet/Day2Q2.1/ClassLibrarylatest/ClassLibrarylatest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrarylatest
+{
+    public class mymmath
+    {
+        public int sqr(int val)
+        {
+            return val * val;
+        }
+
+
+        public int cube(int val)
+        {
+            return val * val * val;
+        }
+    }
+}
