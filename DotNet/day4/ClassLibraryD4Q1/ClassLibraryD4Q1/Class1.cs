cf8f10675a992a9081139745f5a4896e94f7c372
Deepak Ashokrao Jagtap <63543321+Deepakj267@users.noreply.github.com>
2020-10-22 21:25:16 +0530

    Add files via upload

---
+++ DotNet/day4/ClassLibraryD4Q1/ClassLibraryD4Q1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassLibraryD4Q1
+{
+    public class account
+    {
+        private int id;
+        private String Name;
+        private double balanceamt=1000;
+
+        public account(int id, string Name)
+        {
+            this.id = id;
+            this.Name = Name;
+        }
+        public void deposite(double amt)
+        {
+            balanceamt = balanceamt + amt;
+            Console.WriteLine("balance deposited={0}", amt);
+            Console.WriteLine("total balance ={0}", balanceamt);
+        }
+        public void withdrow(double wval)
+        {
+            balanceamt = balanceamt - wval;
+            Console.WriteLine("balance withdrow={0}", wval);
+            Console.WriteLine("total balance ={0}", balanceamt);
+        }
+    }
+}
